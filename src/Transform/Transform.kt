dca7a42ff2ead413f66178541b83b7da942679ec
Salman <salman@shapesecurity.com>
2017-05-15 09:21:25 -0700

    prenex normal form

--- src/Transform/Transform.kt
+++ src/Transform/Transform.kt
@@ -2,6 +2,9 @@ package Transform
 
 import Formula.*
 
+private val INVALID_TERM_EXCEPTION = "Internal Error: Invalid Term"
+private val INVALID_FORMULA_EXCEPTION = "Internal Error: Invalid Formula"
+
 /**
  * Applies a substitution function on the term.
  */
@@ -9,7 +12,7 @@ fun Term.substitute(substitutions: (Var) -> Term): Term {
     return when (this) {
         is Var -> substitutions(this)
         is App -> App(this.function, this.terms.map { it.substitute(substitutions) })
-        else -> throw RuntimeException("Internal Error: Invalid Term")
+        else -> throw RuntimeException(INVALID_TERM_EXCEPTION)
     }
 }
 
@@ -28,7 +31,7 @@ fun Formula.substitute(substitutions: (Var) -> Term): Formula {
         is Implies -> Implies(this.left.substitute(substitutions), this.right.substitute(substitutions))
         is Exists -> Exists(this.variables, this.formula.substitute(substitutions))
         is Forall -> Forall(this.variables, this.formula.substitute(substitutions))
-        else -> throw RuntimeException("Internal Error: Invalid Formula")
+        else -> throw RuntimeException(INVALID_FORMULA_EXCEPTION)
     }
 }
 
@@ -39,7 +42,7 @@ fun Term.renameVar(renaming: (Var) -> Var): Term {
     return when (this) {
         is Var -> renaming(this)
         is App -> App(this.function, this.terms.map { it.substitute(renaming) })
-        else -> throw RuntimeException("Internal Error: Invalid Term")
+        else -> throw RuntimeException(INVALID_TERM_EXCEPTION)
     }
 }
 
@@ -58,6 +61,113 @@ fun Formula.renameVar(renaming: (Var) -> Var): Formula {
         is Implies -> Implies(this.left.renameVar(renaming), this.right.renameVar(renaming))
         is Exists -> Exists(this.variables.map(renaming), this.formula.renameVar(renaming))
         is Forall -> Forall(this.variables.map(renaming), this.formula.renameVar(renaming))
+        else -> throw RuntimeException(INVALID_FORMULA_EXCEPTION)
+    }
+}
+
+/**
+ * Converts a formula to a prenex normal form.
+ */
+fun Formula.prenex(): Formula {
+    fun renameVar(variable: Var, noColisionVariableList: Set<Var>): Var {
+        var name = variable.name
+        val names = noColisionVariableList.map(Var::name)
+        while (name in names) {
+            name += "'"
+        }
+        return Var(name)
+    }
+
+    return when (this) {
+        is Top -> Top
+        is Bottom -> Bottom
+        is Atom -> this
+        is Equals -> this
+        is Not -> {
+            val f = formula.prenex()
+            when (f) {
+                is Forall -> Exists(f.variables, Not(f.formula).prenex())
+                is Exists -> Forall(f.variables, Not(f.formula).prenex())
+                else -> Not(f)
+            }
+        }
+        is And -> {
+            val l = left.prenex()
+            val r = right.prenex()
+            if (l is Forall) {
+                val sharedVariables = l.variables.intersect(r.freeVars)
+                val allVariables = (l.variables + freeVars).toSet()
+                val renamedL = l.renameVar { if (sharedVariables.contains(it)) renameVar(it, allVariables) else it }
+                Forall((renamedL as Forall).variables, And(renamedL.formula, r).prenex())
+            } else if (r is Forall) {
+                val sharedVariables = r.variables.intersect(l.freeVars)
+                val allVariables = (r.variables + freeVars).toSet()
+                val renamedR = r.renameVar { if (sharedVariables.contains(it)) renameVar(it, allVariables) else it }
+                Forall((renamedR as Forall).variables, And(l, renamedR.formula).prenex())
+            } else if (l is Exists) {
+                val sharedVariables = l.variables.intersect(r.freeVars)
+                val allVariables = (l.variables + freeVars).toSet()
+                val renamedL = l.renameVar { if (sharedVariables.contains(it)) renameVar(it, allVariables) else it }
+                Exists((renamedL as Exists).variables, And(renamedL.formula, right).prenex())
+            } else if (r is Exists) {
+                val sharedVariables = r.variables.intersect(l.freeVars)
+                val allVariables = (r.variables + freeVars).toSet()
+                val renamedR = r.renameVar { if (sharedVariables.contains(it)) renameVar(it, allVariables) else it }
+                Exists((renamedR as Exists).variables, And(l, renamedR.formula).prenex())
+            } else And(l, r)
+        }
+        is Or -> {
+            val l = left.prenex()
+            val r = right.prenex()
+            if (l is Forall) {
+                val sharedVariables = l.variables.intersect(r.freeVars)
+                val allVariables = (l.variables + freeVars).toSet()
+                val renamedL = l.renameVar { if (sharedVariables.contains(it)) renameVar(it, allVariables) else it }
+                Forall((renamedL as Forall).variables, Or(renamedL.formula, r).prenex())
+            } else if (r is Forall) {
+                val sharedVariables = r.variables.intersect(l.freeVars)
+                val allVariables = (r.variables + freeVars).toSet()
+                val renamedR = r.renameVar { if (sharedVariables.contains(it)) renameVar(it, allVariables) else it }
+                Forall((renamedR as Forall).variables, Or(l, renamedR.formula).prenex())
+            } else if (l is Exists) {
+                val sharedVariables = l.variables.intersect(r.freeVars)
+                val allVariables = (l.variables + freeVars).toSet()
+                val renamedL = l.renameVar { if (sharedVariables.contains(it)) renameVar(it, allVariables) else it }
+                Exists((renamedL as Exists).variables, Or(renamedL.formula, right).prenex())
+            } else if (r is Exists) {
+                val sharedVariables = r.variables.intersect(l.freeVars)
+                val allVariables = (r.variables + freeVars).toSet()
+                val renamedR = r.renameVar { if (sharedVariables.contains(it)) renameVar(it, allVariables) else it }
+                Exists((renamedR as Exists).variables, Or(l, renamedR.formula).prenex())
+            } else Or(l, r)
+        }
+        is Implies -> {
+            val l = left.prenex()
+            val r = right.prenex()
+            if (l is Forall) {
+                val sharedVariables = l.variables.intersect(r.freeVars)
+                val allVariables = (l.variables + freeVars).toSet()
+                val renamedL = l.renameVar { if (sharedVariables.contains(it)) renameVar(it, allVariables) else it }
+                Exists((renamedL as Forall).variables, Implies(renamedL.formula, r).prenex())
+            } else if (r is Forall) {
+                val sharedVariables = r.variables.intersect(l.freeVars)
+                val allVariables = (r.variables + freeVars).toSet()
+                val renamedR = r.renameVar { if (sharedVariables.contains(it)) renameVar(it, allVariables) else it }
+                Forall((renamedR as Forall).variables, Implies(l, renamedR.formula).prenex())
+            } else if (l is Exists) {
+                val sharedVariables = l.variables.intersect(r.freeVars)
+                val allVariables = (l.variables + freeVars).toSet()
+                val renamedL = l.renameVar { if (sharedVariables.contains(it)) renameVar(it, allVariables) else it }
+                Forall((renamedL as Exists).variables, Implies(renamedL.formula, right).prenex())
+            } else if (r is Exists) {
+                val sharedVariables = r.variables.intersect(l.freeVars)
+                val allVariables = (r.variables + freeVars).toSet()
+                val renamedR = r.renameVar { if (sharedVariables.contains(it)) renameVar(it, allVariables) else it }
+                Exists((renamedR as Exists).variables, Implies(l, renamedR.formula).prenex())
+            } else Implies(l, r)
+        }
+        is Forall -> Forall(variables, formula.prenex())
+        is Exists -> Exists(variables, formula.prenex())
         else -> throw RuntimeException("Internal Error: Invalid Formula")
     }
 }
--- test/Transform/TransformTest.kt
+++ test/Transform/TransformTest.kt
@@ -49,7 +49,7 @@ internal class TransformTest {
         })
         assertEquals("Internal Error: Invalid Term", try {
             INVALID_TERM.renameVar { it }
-        } catch (e: Exception){
+        } catch (e: Exception) {
             e.message
         })
     }
@@ -147,7 +147,7 @@ internal class TransformTest {
 
         assertEquals("Internal Error: Invalid Formula", try {
             INVALID_FORMULA.renameVar { it }
-        } catch (e: Exception){
+        } catch (e: Exception) {
             e.message
         })
     }
@@ -207,7 +207,7 @@ internal class TransformTest {
         })
         assertEquals("Internal Error: Invalid Term", try {
             INVALID_TERM.substitute { it }
-        } catch (e: Exception){
+        } catch (e: Exception) {
             e.message
         })
     }
@@ -304,7 +304,87 @@ internal class TransformTest {
                 })
         assertEquals("Internal Error: Invalid Formula", try {
             INVALID_FORMULA.substitute { it }
-        } catch (e: Exception){
+        } catch (e: Exception) {
+            e.message
+        })
+    }
+
+    @Test
+    fun prenex() {
+        assertEquals(TRUE, TRUE.prenex())
+        assertEquals(FALSE, FALSE.prenex())
+        assertEquals(P(x), P(x).prenex())
+        assertEquals(x equals y, (x equals y).prenex())
+        assertEquals(!P(x), (!P(x)).prenex())
+        assertEquals(P(x) and Q(y), (P(x) and Q(y)).prenex())
+        assertEquals(P(x) or Q(y), (P(x) or Q(y)).prenex())
+        assertEquals(P(x) implies Q(y), (P(x) implies Q(y)).prenex())
+        assertEquals(exists(x) { P(x) and !Q(y) or R(z) }, exists(x) { P(x) and !Q(y) or R(z) }.prenex())
+        assertEquals(forall(x) { P(x) and !Q(y) or R(z) }, forall(x) { P(x) and !Q(y) or R(z) }.prenex())
+
+        assertEquals(forall(x) { !P(x) }, (!exists(x) { P(x) }).prenex())
+        assertEquals(forall(x) { P(x) and Q(y) }, (forall(x) { P(x) } and Q(y)).prenex())
+        assertEquals(exists(x) { P(x) and Q(y) }, (exists(x) { P(x) } and Q(y)).prenex())
+        assertEquals(forall(x_1) { P(x_1) and Q(x) }, (forall(x) { P(x) } and Q(x)).prenex())
+        assertEquals(exists(x_1) { P(x_1) and Q(x) }, (exists(x) { P(x) } and Q(x)).prenex())
+        assertEquals(exists(x_1, y_1) { P(x_1, y_1) and Q(x, y) }, (exists(x, y) { P(x, y) } and Q(x, y)).prenex())
+        assertEquals(forall(x) { Q(y) and P(x) }, (Q(y) and forall(x) { P(x) }).prenex())
+        assertEquals(exists(x) { Q(y) and P(x) }, (Q(y) and exists(x) { P(x) }).prenex())
+        assertEquals(forall(x_1) { Q(x) and P(x_1) }, (Q(x) and forall(x) { P(x) }).prenex())
+        assertEquals(exists(x_1) { Q(x) and P(x_1) }, (Q(x) and exists(x) { P(x) }).prenex())
+        assertEquals(exists(x_1, y_1) { Q(x, y) and P(x_1, y_1) }, (Q(x, y) and exists(x, y) { P(x, y) }).prenex())
+        assertEquals(forall(x) { P(x) or Q(y) }, (forall(x) { P(x) } or Q(y)).prenex())
+        assertEquals(exists(x) { P(x) or Q(y) }, (exists(x) { P(x) } or Q(y)).prenex())
+        assertEquals(forall(x_1) { P(x_1) or Q(x) }, (forall(x) { P(x) } or Q(x)).prenex())
+        assertEquals(exists(x_1) { P(x_1) or Q(x) }, (exists(x) { P(x) } or Q(x)).prenex())
+        assertEquals(exists(x_1, y_1) { P(x_1, y_1) or Q(x, y) }, (exists(x, y) { P(x, y) } or Q(x, y)).prenex())
+        assertEquals(forall(x) { Q(y) or P(x) }, (Q(y) or forall(x) { P(x) }).prenex())
+        assertEquals(exists(x) { Q(y) or P(x) }, (Q(y) or exists(x) { P(x) }).prenex())
+        assertEquals(forall(x_1) { Q(x) or P(x_1) }, (Q(x) or forall(x) { P(x) }).prenex())
+        assertEquals(exists(x_1) { Q(x) or P(x_1) }, (Q(x) or exists(x) { P(x) }).prenex())
+        assertEquals(exists(x_1, y_1) { Q(x, y) or P(x_1, y_1) }, (Q(x, y) or exists(x, y) { P(x, y) }).prenex())
+        assertEquals(exists(x) { P(x) implies Q(y) }, (forall(x) { P(x) } implies Q(y)).prenex())
+        assertEquals(forall(x) { P(x) implies Q(y) }, (exists(x) { P(x) } implies Q(y)).prenex())
+        assertEquals(exists(x_1) { P(x_1) implies Q(x) }, (forall(x) { P(x) } implies Q(x)).prenex())
+        assertEquals(forall(x_1) { P(x_1) implies Q(x) }, (exists(x) { P(x) } implies Q(x)).prenex())
+        assertEquals(forall(x_1, y_1) { P(x_1, y_1) implies Q(x, y) }, (exists(x, y) { P(x, y) } implies Q(x, y)).prenex())
+        assertEquals(forall(x) { Q(y) implies P(x) }, (Q(y) implies forall(x) { P(x) }).prenex())
+        assertEquals(exists(x) { Q(y) implies P(x) }, (Q(y) implies exists(x) { P(x) }).prenex())
+        assertEquals(forall(x_1) { Q(x) implies P(x_1) }, (Q(x) implies forall(x) { P(x) }).prenex())
+        assertEquals(exists(x_1) { Q(x) implies P(x_1) }, (Q(x) implies exists(x) { P(x) }).prenex())
+        assertEquals(exists(x_1, y_1) { Q(x, y) implies P(x_1, y_1) }, (Q(x, y) implies exists(x, y) { P(x, y) }).prenex())
+//        //renaming tests
+        assertEquals(forall(x_2, x_1) { P(x_2) and Q(x) }, (forall(x, x_1) { P(x) } and Q(x)).prenex())
+        assertEquals(exists(x_2, x_1) { P(x_2) and Q(x) }, (exists(x, x_1) { P(x) } and Q(x)).prenex())
+        assertEquals(exists(x_2) { P(x_2) and Q(x, x_1) }, (exists(x) { P(x) } and Q(x, x_1)).prenex())
+        assertEquals(exists(x_2) { P(x_2, x_1) and Q(x) }, (exists(x) { P(x, x_1) } and Q(x)).prenex())
+        assertEquals(forall(x_2, x_1) { Q(x) and P(x_2) }, (Q(x) and forall(x, x_1) { P(x) }).prenex())
+        assertEquals(exists(x_2, x_1) { Q(x) and P(x_2) }, (Q(x) and exists(x, x_1) { P(x) }).prenex())
+        assertEquals(exists(x_2) { Q(x, x_1) and P(x_2) }, (Q(x, x_1) and exists(x) { P(x) }).prenex())
+        assertEquals(exists(x_2) { Q(x) and P(x_2, x_1) }, (Q(x) and exists(x) { P(x, x_1) }).prenex())
+        assertEquals(forall(x_2, x_1) { P(x_2) or Q(x) }, (forall(x, x_1) { P(x) } or Q(x)).prenex())
+        assertEquals(exists(x_2, x_1) { P(x_2) or Q(x) }, (exists(x, x_1) { P(x) } or Q(x)).prenex())
+        assertEquals(exists(x_2) { P(x_2) or Q(x, x_1) }, (exists(x) { P(x) } or Q(x, x_1)).prenex())
+        assertEquals(exists(x_2) { P(x_2, x_1) or Q(x) }, (exists(x) { P(x, x_1) } or Q(x)).prenex())
+        assertEquals(forall(x_2, x_1) { Q(x) or P(x_2) }, (Q(x) or forall(x, x_1) { P(x) }).prenex())
+        assertEquals(exists(x_2, x_1) { Q(x) or P(x_2) }, (Q(x) or exists(x, x_1) { P(x) }).prenex())
+        assertEquals(exists(x_2) { Q(x, x_1) or P(x_2) }, (Q(x, x_1) or exists(x) { P(x) }).prenex())
+        assertEquals(exists(x_2) { Q(x) or P(x_2, x_1) }, (Q(x) or exists(x) { P(x, x_1) }).prenex())
+        assertEquals(exists(x_2, x_1) { P(x_2) implies Q(x) }, (forall(x, x_1) { P(x) } implies Q(x)).prenex())
+        assertEquals(forall(x_2, x_1) { P(x_2) implies Q(x) }, (exists(x, x_1) { P(x) } implies Q(x)).prenex())
+        assertEquals(forall(x_2) { P(x_2) implies Q(x, x_1) }, (exists(x) { P(x) } implies Q(x, x_1)).prenex())
+        assertEquals(forall(x_2) { P(x_2, x_1) implies Q(x) }, (exists(x) { P(x, x_1) } implies Q(x)).prenex())
+        assertEquals(forall(x_2, x_1) { Q(x) implies P(x_2) }, (Q(x) implies forall(x, x_1) { P(x) }).prenex())
+        assertEquals(exists(x_2, x_1) { Q(x) implies P(x_2) }, (Q(x) implies exists(x, x_1) { P(x) }).prenex())
+        assertEquals(exists(x_2) { Q(x, x_1) implies P(x_2) }, (Q(x, x_1) implies exists(x) { P(x) }).prenex())
+        assertEquals(exists(x_2) { Q(x) implies P(x_2, x_1) }, (Q(x) implies exists(x) { P(x, x_1) }).prenex())
+
+        assertEquals(forall(z) { forall(x) { (P() or Q(x)) implies R(z) } }, ((P() or exists(x) { Q(x) }) implies forall(z) { R(z) }).prenex())
+        //assertEquals("", ((P() or exists(y) { Q(y) }) implies forall(z) { R(z) }).prenex().print())
+
+        assertEquals("Internal Error: Invalid Formula", try {
+            INVALID_FORMULA.prenex()
+        } catch (e: Exception) {
             e.message
         })
     }
